c026ef6797afc340229d07468163ea04ee787365
henristr <strickerhenri@gmail.com>
2025-11-19 14:21:50 +0100

    improved project structure

--- app.py
+++ app.py
@@ -284,4 +284,4 @@ def api_generate_recipe():
 
 # ------------------ MAIN ------------------
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8888, debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
